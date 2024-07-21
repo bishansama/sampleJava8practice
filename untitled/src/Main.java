caab050c78b845447cbeff1e0f9a8caa7214501e
bishan.sama <bishan.sama@gmail.com>
2024-07-21 14:27:13 +0530

    first commit

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,9 +1,21 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Employee> employees = new ArrayList<>();
+
+
+        employees.add(new Employee("John", "Doe", 1000));
+        employees.add(new Employee("Jane", "Doe", 2000));
+        employees.add(new Employee("Jim", "Doe", 3000));
+        employees.add(new Employee("Jill", "Doe", 4000));
+        employees.add(new Employee("Jack", "Doe", 5000));
+
+        List<Employee> employees2 = employees.stream().filter(employee -> employee.getSalary() > 3000).toList();
+
+        employees2.forEach(System.out::println);
 
 
     }
